d0f0912d6c48cde25aa5882860c53711ff358969
Arkashersz <extensed.wc@hotmail.com>
2024-05-01 11:08:48 -0300

    Criação da estilização da interface

--- src/main/java/com/jmc/kwikbank/Controllers/LoginController.java
+++ src/main/java/com/jmc/kwikbank/Controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.jmc.kwikbank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    public ChoiceBox acc_selector;
+    public Label id_lbl;
+    public TextField id_fld;
+    public Label password_lbl;
+    public TextField password_fld;
+    public Button login_btn;
+    public Label error_lbl;
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,15 @@
-module com.jmc.kwikbank.kwikbank {
+module com.jmc.kwikbank {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
-
-    opens com.jmc.kwikbank.kwikbank to javafx.fxml;
-    exports com.jmc.kwikbank.kwikbank;
+    opens com.jmc.kwikbank to javafx.fxml;
+    exports com.jmc.kwikbank;
+    exports com.jmc.kwikbank.Controllers;
+    exports com.jmc.kwikbank.Controllers.Admin;
+    exports com.jmc.kwikbank.Controllers.Client;
+    exports com.jmc.kwikbank.Models;
+    exports com.jmc.kwikbank.Views;
 }
