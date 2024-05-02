f524c5c51a179e638f9ab05940f61d3284651ddb
Arkashersz <extensed.wc@hotmail.com>
2024-05-01 21:14:05 -0300

    Habilitando login para visualização do painel do usuário

--- src/main/java/com/jmc/kwikbank/Controllers/LoginController.java
+++ src/main/java/com/jmc/kwikbank/Controllers/LoginController.java
@@ -1,11 +1,17 @@
 package com.jmc.kwikbank.Controllers;
 
+import com.jmc.kwikbank.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox acc_selector;
     public Label id_lbl;
     public TextField id_fld;
@@ -13,4 +19,16 @@ public class LoginController {
     public TextField password_fld;
     public Button login_btn;
     public Label error_lbl;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_btn.setOnAction(event -> onLogin());
+    }
+
+    private void onLogin() {
+        Stage stage = (Stage) error_lbl.getScene().getWindow();
+        Model.getInstance().getViewFactory().closeStage(stage);
+        Model.getInstance().getViewFactory().showClientWindow();
+    }
 }
--- src/main/java/com/jmc/kwikbank/Models/Model.java
+++ src/main/java/com/jmc/kwikbank/Models/Model.java
@@ -1,4 +1,23 @@
 package com.jmc.kwikbank.Models;
 
+import com.jmc.kwikbank.Views.ViewFactory;
+
 public class Model {
+    private static Model model;
+    private final ViewFactory viewFactory;
+
+    private Model() {
+        this.viewFactory = new ViewFactory();
+    }
+
+    public static synchronized Model getInstance() {
+        if (model == null) {
+            model = new Model();
+        }
+        return model;
+    }
+
+    public ViewFactory getViewFactory() {
+        return viewFactory;
+    }
 }
