0cafcfcf53d892f8c028c6f6741f45325d9dbdb9
Arkashersz <extensed.wc@hotmail.com>
2024-05-04 10:05:51 -0300

    Criação do design da lista de transações

---
+++ src/main/java/com/jmc/kwikbank/Controllers/Client/TransactionCellController.java
@@ -0,0 +1,21 @@
+package com.jmc.kwikbank.Controllers.Client;
+
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionCellController implements Initializable {
+
+    public FontAwesomeIconView in_icon;
+    public FontAwesomeIconView out_icon;
+    public Label trans_date_lbl;
+    public Label sender_lbl;
+    public Label receive_lbl;
+    public Label amount_lbl;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+}
