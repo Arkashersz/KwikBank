64773b9e5010adeb12d3967f53e9ef9fe751da52
Arkashersz <extensed.wc@hotmail.com>
2024-05-06 17:45:28 -0300

    Habilitado função para cadastrar novos clientes no painel admin

--- src/main/java/com/jmc/kwikbank/Controllers/Admin/CreateClientController.java
+++ src/main/java/com/jmc/kwikbank/Controllers/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package com.jmc.kwikbank.Controllers.Admin;
 
+import com.jmc.kwikbank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -22,6 +25,85 @@ public class CreateClientController implements Initializable {
     public CheckBox pAddress_box;
     public Label error_lbl;
 
+    private String payeeAddress;
+    private boolean createCheckingAccountFlag = false;
+    private boolean createSavingsAccountFlag = false;
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        create_client_btn.setOnAction(event -> createClient());
+        pAddress_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                payeeAddress = createPayeeAddress();
+                onCreatePayeeAddress();
+            }
+        });
+        ch_acc_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                createCheckingAccountFlag = true;
+            }
+        });
+        sv_acc_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                createSavingsAccountFlag = true;
+            }
+        });
+
+    }
+
+    private void createClient() {
+        if(createCheckingAccountFlag) {
+            createAccount("Checking");
+        }
+
+        if (createSavingsAccountFlag) {
+            createAccount("Savings");
+        }
+
+        String fName = fName_fld.getText();
+        String lName = lName_fld.getText();
+        String password = password_fld.getText();
+        Model.getInstance().getDatabaseDriver().createClient(fName, lName, payeeAddress, password, LocalDate.now());
+        error_lbl.setStyle("-fx-text-fill: blue; -fx-font-size: 1.3em; -fx-font-weight: bold");
+        error_lbl.setText("Cliente cadastrado com sucesso!");
+        emptyFields();
+    }
+
+    private void createAccount(String accountType) {
+        double balance = Double.parseDouble(ch_amount_fld.getText());
+        // Criar o número da conta
+        String firstSection = "3201";
+        String lastSection = Integer.toString((new Random()).nextInt(999) + 1000);
+        String accountNumber = firstSection + " " + lastSection;
+        // Criando a conta corrente ou poupança
+        if(accountType.equals("Checking")) {
+            Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress, accountNumber, 10, balance);
+        } else {
+            Model.getInstance().getDatabaseDriver().createSavingsAccount(payeeAddress, accountNumber, 2000, balance);
+        }
+    }
+
+    private void onCreatePayeeAddress() {
+        if (fName_fld.getText() != null & lName_fld.getText() != null) {
+            pAddress_lbl.setText(payeeAddress);
+        }
+    }
+
+    private String createPayeeAddress() {
+        int id = Model.getInstance().getDatabaseDriver().getLastClientsId() + 1;
+        char fChar = Character.toLowerCase(fName_fld.getText().charAt(0));
+        return "@"+fChar+lName_fld.getText()+id;
+    }
+
+    private void emptyFields() {
+        fName_fld.setText("");
+        lName_fld.setText("");
+        password_fld.setText("");
+        pAddress_box.setSelected(false);
+        pAddress_lbl.setText("");
+        ch_acc_box.setSelected(false);
+        ch_amount_fld.setText("");
+        sv_acc_box.setSelected(false);
+        sv_amount_fld.setText("");
+    }
 }
--- src/main/java/com/jmc/kwikbank/Models/DatabaseDriver.java
+++ src/main/java/com/jmc/kwikbank/Models/DatabaseDriver.java
@@ -1,6 +1,7 @@
 package com.jmc.kwikbank.Models;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseDriver {
     private Connection conn;
@@ -44,7 +45,56 @@ public class DatabaseDriver {
         return resultSet;
     }
 
+    public void createClient(String fName, String lName, String pAddress, String password, LocalDate date) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "Clients (FirstName, LastName, PayeeAddress, Password, Date)" +
+                    "VALUES ('"+fName+"', '"+lName+"', '"+pAddress+"', '"+password+"', '"+date.toString()+"');");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createCheckingAccount(String owner, String number, double tLimit, double balance) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "CheckingAccounts (Owner, AccountNumber, TransactionLimit, Balance)" +
+                    " VALUES ('"+owner+"', '"+number+"', "+tLimit+", "+balance+")");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void createSavingsAccount(String owner, String number, double wLimit, double balance) {
+        Statement statement;
+        try {
+            statement = this.conn.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "SavingsAccounts (Owner, AccountNumber, WithdrawalLimit, Balance)" +
+                    " VALUES ('"+owner+"', '"+number+"', "+wLimit+", "+balance+")");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
     /*
     *Métodos Úteis
      */
+    public int getLastClientsId() {
+        Statement statement;
+        ResultSet resultSet;
+        int id = 0;
+        try {
+            statement = this.conn.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM sqlite_sequence WHERE name='Clients';");
+            id = resultSet.getInt("seq");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
 }
